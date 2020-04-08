efc0bfe2b667aea94b69f081223756d7059a4ab2
Ian Handel <ian.handel@ed.ac.uk>
2020-04-08 09:21:24 +0100

    update

--- update.R
+++ update.R
@@ -1,6 +1,9 @@
 library(readr)
+library(dplyr)
 
 dat <- read_csv("https://raw.githubusercontent.com/ianhandel/test_data/master/test.csv", )
 
-dat <- bind_rows(dat, data.frame(date = date())) 
+dat <- dplyr::bind_rows(dat, data.frame(date = date())) %>% 
+  select(date)
 
+write_csv(dat, "test.csv")
